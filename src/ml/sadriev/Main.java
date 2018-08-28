937752e7272abf7a22a14e6a1f608d2a35f2b39c
sharafan home terminal <sharafanmaxim78@gmail.com>
2018-08-28 15:13:55 +0300

    fillArray method

--- src/ml/sadriev/Main.java
+++ src/ml/sadriev/Main.java
@@ -1,8 +1,22 @@
 package ml.sadriev;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    }
+
+    private static int[] fillArray() {
+        int[] array = new int[20];
+
+        Random random = new Random(8346234L);
+
+        for (int i = 0; i < 20; i++) {
+            array[i] = (int) Math.round(100 * Math.random());
+        }
+
+        return array;
     }
 }
